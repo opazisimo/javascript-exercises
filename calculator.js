68a266f216cd1cc4862ba5e6a6a218df57ef2ebd
Susana Opazo <sopazo@thoughtworks.com>
2019-02-07 09:31:51 -0300

    added multiply and some refactoring 🤘

--- calculator.js
+++ calculator.js
@@ -2,6 +2,16 @@ class Calculator {
   sum(addend1, addend2){
     return addend1 + addend2
   }
+
+  multiply(number1, number2){
+    let result = 0
+
+    for (var i = 0; i < Math.abs(number2); i++) {
+      result = this.sum(result, number1)
+    }
+
+    return result
+  }
 }
 
 module.exports = Calculator
--- calculator.test.js
+++ calculator.test.js
@@ -1,23 +1,31 @@
 const Calculator = require("./calculator")
 
 it("add 8 and 2 numbers", () => {
-  const addend1 = 8
-  const addend2 = 2
   const myCalculator = new Calculator()
-
-  const result = myCalculator.sum(addend1, addend2)
-
+  const result = myCalculator.sum(8, 2)
   expect(result).toEqual(10)
-
 })
 
 it("adds 3 and 28 numbers", () => {
-  const addend1 = 3
-  const addend2 = 28
   const myCalculator = new Calculator()
-
-  const result = myCalculator.sum(addend1, addend2)
-
+  const result = myCalculator.sum(3, 28)
   expect(result).toEqual(31)
+})
 
+it("multiply 2 and 3", () => {
+  const myCalculator = new Calculator()
+  const result = myCalculator.multiply(2, 3)
+  expect(result).toEqual(6)
+})
+
+it("multiply 60 and 4", () => {
+  const myCalculator = new Calculator()
+  const result = myCalculator.multiply(60, 4)
+  expect(result).toEqual(240)
+})
+
+it("multiply with one negative number", () => {
+  const myCalculator = new Calculator()
+  const result = myCalculator.multiply(-2, 3)
+  expect(result).toEqual(-6)
 })
